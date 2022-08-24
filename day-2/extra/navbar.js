57ba98332f174c60296259f5845d8f3450056d65
Nishant22 <iamnishant228@gmail.com>
2022-08-24 20:07:27 +0530

    day2 files added
    day2 files added

---
+++ day-2/extra/navbar.js
@@ -0,0 +1,4 @@
+let cart_slider = document.getElementById("cart_slider");
+let cart = document.getElementById("cart").addEventListener("click" , function () {
+    cart_slider.style.display = "block";
+})
